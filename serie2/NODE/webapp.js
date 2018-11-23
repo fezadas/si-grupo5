e545f198828a5255c08f1dc91d5935d68404f3c5
fezadas <filipe.machado.fe@gmail.com>
2018-11-23 15:06:00 +0000

    FINISHED!!

--- serie2/NODE/webapp.js
+++ serie2/NODE/webapp.js
@@ -6,6 +6,8 @@ const fs = require('fs')
 
 const port = 3001
 
+const crypto = require('crypto');
+
 app.set('view engine', 'hbs')
 hbs.registerPartials('./views/partials')
 
@@ -17,10 +19,14 @@ const DROPBOX_KEY = process.env.DROPBOX_KEY
 const DROPBOX_SECRET = process.env.DROPBOX_SECRET
 
 const DROPBOX_COOKIE_ID = 'idDropBox'
+const DROPBOX_TAG_ID = "tagDropbox"
 const DRIVE_COOKIE_ID = 'idDrive'
+const DRIVE_TAG_ID = "tagDrive"
 
 let map_access_token = {}
-let map_dropbox_access_token = {};
+let map_dropbox_access_token = {}
+let session_key 
+let states = {}
 
 app.listen(port, (err) => {
     if (err) {
@@ -33,7 +39,12 @@ app.get('/', (req, resp) => {
     resp.render('home.hbs')
 })
 
+
 app.get('/login', (req, resp) => {
+
+    let session_id = Math.random()
+    states[session_id] = true
+
     resp.redirect(302,
     // authorization endpoint
     'https://accounts.google.com/o/oauth2/v2/auth?'
@@ -41,13 +52,17 @@ app.get('/login', (req, resp) => {
     + 'client_id='+ CLIENT_ID +'&'
     // scope "openid email"
     + 'scope=openid%20email%20https://www.googleapis.com/auth/drive.readonly&'
+    + `state=${session_id}&`
     // responde_type for "authorization code grant"
     + 'response_type=code&'
     // redirect uri used to register RP
-    + 'redirect_uri=http://localhost:3001/googlecallback')
+    + 'redirect_uri=http://localhost:3001/googlecallback') //state
 })
 
 app.get('/googlecallback', (req, resp) => {
+    let key = req.query.state
+    if(!states[key]) 
+        return resp.redirect(302,'/login')
     console.log('making request to token endpoint')
     request
         .post(
@@ -65,16 +80,27 @@ app.get('/googlecallback', (req, resp) => {
                 console.log(body);
                 var json_response = JSON.parse(body);
                 let id = Math.random()
-                resp.setHeader('Set-Cookie', [DRIVE_COOKIE_ID+"="+id]);
-                map_access_token[id]=json_response.access_token                
-                resp.redirect(302,'https://www.dropbox.com/oauth2/authorize?client_id='+DROPBOX_KEY+
-                '&response_type=code&redirect_uri=http://localhost:3001/dropboxcallback')
+
+                let h = crypto.createHmac('sha256', 'si-serie-2').digest(id)
+                // convert to base64
+                let hBase64 = Buffer.from(h).toString('base64');
+
+                resp.setHeader('Set-Cookie', [DRIVE_COOKIE_ID+"="+id, DRIVE_TAG_ID + "=" + hBase64]);
+                map_access_token[id]={
+                    "token" : json_response.access_token,
+                    "tag" : hBase64
+                }                
+                resp.redirect(302,'https://www.dropbox.com/oauth2/authorize?client_id='+ DROPBOX_KEY +
+                '&response_type=code&'+ `state=${key}&`+'redirect_uri=http://localhost:3001/dropboxcallback')
             }
         );
 })
 
 app.get('/dropboxcallback',(req,resp) => {
-
+    let key = req.query.state
+    if(!states[key]) 
+        return resp.redirect(302,'/login')
+    delete states[key]
     request
         .post(
             { 
@@ -91,26 +117,37 @@ app.get('/dropboxcallback',(req,resp) => {
                 console.log(body);
                 var json_response = JSON.parse(body);
                 let id = Math.random()
-                resp.setHeader('Set-Cookie', [DROPBOX_COOKIE_ID+"="+id]);
-                map_dropbox_access_token[id]=json_response.access_token
-                resp.redirect(302,'/googleDriveFiles')
-            })    
+
+                let h = crypto.createHmac('sha256', 'si-serie-2').digest(id)
+                // convert to base64
+                let hBase64 = Buffer.from(h).toString('base64');
+
+                resp.setHeader('Set-Cookie', [DROPBOX_COOKIE_ID + "=" + id, DROPBOX_TAG_ID + '='+hBase64]);
+                map_dropbox_access_token[id]={
+                    "token" : json_response.access_token,
+                    "tag" : hBase64
+                }
+                resp.redirect(302,'/googleDriveFiles')            
+        })    
     })
 
 app.get('/googleDriveFiles',(req,resp) => {
 
-    let drive_key = getCookieId(req.headers.cookie,DRIVE_COOKIE_ID)
-
-    if(!drive_key) resp.redirect(302,'/login') //if user doesnt have cookie, must login again
+    if(!verifyCookie(req, DRIVE_COOKIE_ID, DRIVE_TAG_ID,map_access_token)) 
+        return resp.redirect(302,'/login')
+        
+    let drive_key = getCookieId(req.headers.cookie,DRIVE_COOKIE_ID)    
 
     request
             .get({
                 url: 'https://www.googleapis.com/drive/v2/files',
                 headers: {
-                    Authorization: "Bearer "+map_access_token[drive_key]
+                    Authorization: "Bearer "+map_access_token[drive_key].token
                 }   
             },
             (err, res, body) => {
+                if(res.statusCode != 200)
+                   return resp.redirect(302,'/login')
                 var info = JSON.parse(body)
                 var list = info.items.map(file => {
                     return {
@@ -125,6 +162,11 @@ app.get('/googleDriveFiles',(req,resp) => {
 })
 
 app.get('/googleDriveFiles/file',(req,resp) => {
+
+    if(!verifyCookie(req, DROPBOX_COOKIE_ID, DROPBOX_TAG_ID,map_dropbox_access_token) &&
+    !verifyCookie(req, DRIVE_COOKIE_ID, DRIVE_TAG_ID,map_access_token))
+     resp.redirect(302,'/login')
+
     const id = req.query.id
     const name = req.query.name
     resp.render('googlefile.hbs',{id:id,name:name})
@@ -132,17 +174,19 @@ app.get('/googleDriveFiles/file',(req,resp) => {
 
 app.get('/uploadFile/file',(req,resp) => {
 
-    let drive_key = getCookieId(req.headers.cookie,DRIVE_COOKIE_ID)
-    let dropbox_key = getCookieId(req.headers.cookie,DROPBOX_COOKIE_ID)
+    if(!verifyCookie(req, DROPBOX_COOKIE_ID, DROPBOX_TAG_ID,map_dropbox_access_token) &&
+       !verifyCookie(req, DRIVE_COOKIE_ID, DRIVE_TAG_ID,map_access_token))
+        resp.redirect(302,'/login')
 
-    if(!drive_key || !dropbox_key) resp.redirect(302,'/login')
+    let drive_key = getCookieId(req.headers.cookie,DRIVE_COOKIE_ID)
+    let dropbox_key = getCookieId(req.headers.cookie,DROPBOX_COOKIE_ID)    
 
     const id = req.query.id
     request
             .get({
                 url: 'https://www.googleapis.com/drive/v2/files/'+id,
                 headers: {
-                    Authorization: "Bearer "+map_access_token[drive_key]
+                    Authorization: "Bearer "+map_access_token[drive_key].token
                 }   
             },
             (err, res, body) => {
@@ -161,7 +205,7 @@ app.get('/uploadFile/file',(req,resp) => {
                         url: downloadUrl,
                         encoding:null,
                         headers: {
-                            Authorization: "Bearer "+map_access_token[drive_key]
+                            Authorization: "Bearer "+map_access_token[drive_key].token
                         } 
                     },
                     (err, res, body) => {
@@ -169,7 +213,7 @@ app.get('/uploadFile/file',(req,resp) => {
                             console.log('Now the data has been written on the temp file.')
                             fs.readFile(tempFileName, (err, data) => {
                                 if (err) throw err;
-                                uploadDropbox(data,fileName,map_dropbox_access_token[dropbox_key])   
+                                uploadDropbox(data,fileName,map_dropbox_access_token[dropbox_key].token)   
                             resp.redirect(302,'/updateDone')
                             });
                     })
@@ -178,6 +222,11 @@ app.get('/uploadFile/file',(req,resp) => {
 })
 
 app.get('/updateDone',(req,resp) => {
+
+    if(!verifyCookie(req, DROPBOX_COOKIE_ID, DROPBOX_TAG_ID,map_dropbox_access_token) &&
+    !verifyCookie(req, DRIVE_COOKIE_ID, DRIVE_TAG_ID,map_access_token))
+     resp.redirect(302,'/login')
+
     resp.render('updateDone.hbs')
 })
 
@@ -210,4 +259,10 @@ function getCookieId(cookies,id){
     let value = "; " + cookies;
     let parts = value.split("; "+id+"=")
     return parts.pop().split(";").shift()
+}
+
+function verifyCookie(req, cookieId, cookieTag, map){
+    let key = getCookieId(req.headers.cookie,cookieId)
+    let tag = getCookieId(req.headers.cookie,cookieTag)
+    return key && map[key] && map[key].tag == tag
 }
